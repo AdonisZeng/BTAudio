435cb2314b05e602ec2242de10b828cb83c97bb9
zengguangnan <3366488273@qq.com>
2026-07-02 23:34:42 +0800

    feat: initial commit of BTAudio - Bluetooth A2DP Sink connector for Windows 10 2004+
    BT Audio (AudioPlaybackConnector) is a modern, open-source Windows application that manages Bluetooth A2DP Sink connections, providing a notification area icon for easy device pairing and management.

---
+++ translate/gen_pot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FILES="BTAudio.cpp"
+
+COPYRIGHT_HOLDER="Richard Yu <yurichard3839@gmail.com>"
+PKG_NAME="BTAudio"
+
+cd ..
+xgettext -o translate/source/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
